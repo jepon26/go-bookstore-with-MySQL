7e309c8151483642dccbdbf59f665b3aad2074ee
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-05-20 19:13:25 -0600

    initial commit

--- pkg/controller/book-controller.go
+++ pkg/controller/book-controller.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
 	"github.com/ahkil/go-bookstore/pkg/models"
 	"github.com/ahkil/go-bookstore/pkg/utils"
+	"github.com/akhil/go-bookstore/pkg/models"
 	"github.com/gorilla/mux"
 )
 
@@ -23,10 +25,27 @@ func GetBook(w http.ResponseWriter, r *http.Request){
 
 
 func GetBookById(w http.ResponseWriter, r *http.Request){
-	vars := muxVars(r)
-	bookID := vars["bookId"]
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId,0,0)
 	if err != nil {
 		fmt.Println("error while parsing")
 }
+bookDetails, _:= models.GetBookById(ID)
+res, _:= json.Marshal(bookDetails)
+w.Header().Set("Content-Type", "pkgaplication/json")
+w.WriteHeader(http.StatusOK)
+w.Write(res)
 }
+
+
+func CreateBook(w http.ResponseWriter, r *http.Request){
+CreateBook := models.Book{}
+utils.parseBody(r, CreateBook)
+b:= CreateBook.CreateBook()
+res, _:= json.Marshal(b)
+w.WriteHeader(http.StatusOK)
+w.Write(res)
+}
+
+
